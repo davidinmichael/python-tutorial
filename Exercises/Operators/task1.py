b328797884b872f9e647f49d5b4e29da1c3cd70f
Sampul-CodeMine <studysampul@gmail.com>
2023-03-13 23:20:08 +0100

    Solution on some task - Dukeson

--- Exercises/Operators/task1.py
+++ Exercises/Operators/task1.py
@@ -1,2 +1,14 @@
+#!/usr/bin/python3
+
 # Write a Python program that calculates the sum and product of two numbers entered by the user.
 # Write your program below
+
+first, second = input(" Enter two numbers: ").split()
+first = int(first)
+second = int(second)
+summ = first + second
+prod = first * second
+print("{0} and {1}. " \
+      "Their sum is {2} and their product" \
+      " is {3}".format(first, second, summ, prod))
+
--- Exercises/Operators/task2.py
+++ Exercises/Operators/task2.py
@@ -1,2 +1,6 @@
+#!/usr/bin/python3
+
 # Write a Python program that checks whether a number entered by the user is even or odd.
 # Write your program below
+
+print("{}".format(True if int(input("Number: ")) % 2 == 0 else False))
