a107593dc56efdbfd40e68d3a4f1cb25827a56f6
Dukeson Ehigboria O <studysampul@gmail.com>
2023-03-14 18:23:29 +0100

    Some new tasks

---
+++ Exercises/Modules/task3.py
@@ -0,0 +1,6 @@
+# Write a program that defines a function called calculate_distance that takes two sets
+# of coordinates (x1, y1) and (x2, y2) as arguments and calculates the Euclidean distance
+# between them using the math module. Then, ask the user to input the coordinates of two
+# points and print the distance between them.
+
+# Write your program below
