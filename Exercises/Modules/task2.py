640c927f3172ef96f6a4b2c57bde8eea1ba6e717
Code-with-Dave <davidmizzy731@gmail.com>
2023-03-14 16:37:00 +0100

    I added lesson modules and tasks on it

---
+++ Exercises/Modules/task2.py
@@ -0,0 +1,5 @@
+# Write a program that imports the random module and uses the randrange function to generate a random integer
+# between 1 and 100 (inclusive). Then, ask the user to guess the number andgive them feedback on whether their
+# guess was too high, too low, or correct.
+
+# Write your program below
