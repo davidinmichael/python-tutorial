dcae3dc367d9a2c252e2a2ca0e53972588e2af90
Sampul-CodeMine <studysampul@gmail.com>
2023-03-16 15:00:16 +0100

    Solution to all tasks and permissions granted

--- Exercises/Loops/task0.py
+++ Exercises/Loops/task0.py
@@ -1,2 +1,19 @@
+#!/usr/bin/python3
+
 # Write a program that asks the user to enter a series of numbers and then prints out the number of even and odd numbers entered.
 # Write your program below
+
+def acceptval(strchr):
+    while True:
+        try:
+            num = int(input(f"Enter {strchr}: "))
+            return (num)
+        except:
+            print("Numeric value required.")
+
+num = []
+rng = acceptval("Range")
+for i in range(rng):
+    num.append(acceptval("Number"))
+print("{} is {}".format
+
