baa09fe529d0031c67ff62757e083ef87827d63d
Dukeson Ehigboria O <studysampul@gmail.com>
2023-03-18 10:04:58 +0100

    Some solutions to the advanced task

--- Exercises/Advanced Tasks/Ballpark-orders.py
+++ Exercises/Advanced Tasks/Ballpark-orders.py
@@ -1,4 +1,6 @@
-# You and three friends go to a baseball game and you offer to go to the concession stand for everyone.
+#!/usr/bin/python3
+
+# You and three friends go to a baseball game. You offer to go to the concession stand for everyone.
 # They each order one thing, and you do as well. Nachos and Pizza both cost $6.00. A Cheeseburger meal costs $10.
 # Water is $4.00 and Coke is $5.00. Tax is 7%.
 
@@ -17,3 +19,6 @@
 
 # Sample Output 
 # 26.75
+menu = {"Nachos": 6, "Pizza": 6, "Cheeseburger": 10, "Water": 4, "Coke": 5}
+tax = 7/100
+total = 0
