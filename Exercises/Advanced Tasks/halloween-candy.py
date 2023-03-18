5a6d2d98e1c7102deedbbfa5fda0e630737c7049
Dukeson Ehigboria O <studysampul@gmail.com>
2023-03-18 16:34:41 +0100

    Starting on the next advance task soon

--- Exercises/Advanced Tasks/halloween-candy.py
+++ Exercises/Advanced Tasks/halloween-candy.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 # You go trick or treating with a friend and all but three of the houses that you visit are giving out candy.
 # One house that you visit is giving out toothbrushes and two houses are giving out dollar bills. 
 
--- Exercises/Advanced Tasks/new-drivers-license.py
+++ Exercises/Advanced Tasks/new-drivers-license.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 # You have to get a new driver's license and you show up at the office at the same time as 4 other people.
 # The office says that they will see everyone in alphabetical order and it takes 20 minutes for them to process
 #     each new license. All of the agents are available now, and they can each see one customer at a time.
